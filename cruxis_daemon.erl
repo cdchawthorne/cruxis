433e387268dc45334e2fd3faa2001c3d8bf46f97
Christopher Hawthorne <cdchawthorne@gmail.com>
2012-10-02 00:06:21 -0400

    Removed unused variable bindings

--- cruxis_daemon.erl
+++ cruxis_daemon.erl
@@ -7,36 +7,36 @@
 start_server() ->
     gen_server:start({local, cruxis_daemon}, ?MODULE, [], []).
 
-init(Args) ->
+init(_) ->
     {ok, idle}.
 
-handle_call(auto_connect, From, State) ->
+handle_call(auto_connect, _, _) ->
     spawn_monitor(shell_calls, auto_connect, []),
     {reply, success, idle};
-handle_call({connect, {by_number, N}}, From, State) ->
+handle_call({connect, {by_number, N}}, _, _) ->
     spawn_monitor(shell_calls, connect_known_network, [N]),
     {reply, success, idle};
-handle_call({connect, {wpa_from_file, File}}, From, State) ->
+handle_call({connect, {wpa_from_file, File}}, _, _) ->
     spawn_monitor(shell_calls, connect_wpa, [File]),
     {reply, success, idle};
-handle_call({connect, {wpa, Ssid, Key}}, From, State) ->
+handle_call({connect, {wpa, Ssid, Key}}, _, _) ->
     spawn_monitor(shell_calls, connect_wpa, [Ssid, Key]),
     {reply, success, idle};
-handle_call({connect, {wep, Ssid, Key}}, From, State) ->
+handle_call({connect, {wep, Ssid, Key}}, _, _) ->
     spawn_monitor(shell_calls, connect_wep, [Ssid, Key]),
     {reply, success, idle};
-handle_call({connect, {unsecured, Ssid}}, From, State) ->
+handle_call({connect, {unsecured, Ssid}}, _, _) ->
     spawn_monitor(shell_calls, connect_unsecured, [Ssid]),
     {reply, success, idle}.
 
-handle_cast(Request, State) ->
+handle_cast(_, _) ->
     {noreply, idle}.
 
-handle_info(Info, State) ->
+handle_info(_, _) ->
     {noreply, idle}.
 
-terminate(Reason, State) ->
+terminate(_, _) ->
     {}.
 
-code_change(OldVsn, State, Extra) ->
+code_change(_, State, _) ->
     {ok, State}.
